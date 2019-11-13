acabd48c99f60528737f829b4fa3a23404a0eddd
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-11-13 20:59:28 +0700

    add time

--- app/src/main/java/nguyenduchung/ndh/intentchonhinhanh04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/intentchonhinhanh04092019/MainActivity.java
@@ -3,12 +3,23 @@ package nguyenduchung.ndh.intentchonhinhanh04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgHinhChon,imgHinhGoc;
+    ImageButton imgPlay;
+    TextView txtTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgHinhChon=findViewById(R.id.imageviewHinhChon);
+        imgHinhGoc=findViewById(R.id.imageviewHinhGoc);
+        imgPlay=findViewById(R.id.imagebuttonPlay);
+        txtTime=findViewById(R.id.textviewTime);
     }
 }
